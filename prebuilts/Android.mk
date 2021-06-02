eb72781983933cbf5fa0c646bd98bf5d1a9fe356
dhimanparas20 <dhimanparas20@gmail.com>
2021-06-02 15:51:44 +0000

    Init
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

---
+++ prebuilts/Android.mk
@@ -0,0 +1,51 @@
+LOCAL_PATH := $(call my-dir)
+
+# Asus Calculator
+include $(CLEAR_VARS)
+LOCAL_MODULE := AsusCalculator
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := system/priv-app/AsusCalculator/AsusCalculator.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
+LOCAL_OVERRIDES_PACKAGES := Calculator ExactCalculator
+include $(BUILD_PREBUILT)
+
+# Gboard 
+include $(CLEAR_VARS)
+LOCAL_MODULE := Gboard
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := system/priv-app/Gboard/Gboard.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
+LOCAL_OVERRIDES_PACKAGES := keyboard latin aospkeyboard
+include $(BUILD_PREBUILT)
+
+# GcamGo
+include $(CLEAR_VARS)
+LOCAL_MODULE := GCam
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := system/priv-app/Gcam/GCam.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
+LOCAL_OVERRIDES_PACKAGES := SnapdragonCamera Snap Camera2
+include $(BUILD_PREBUILT)
+
+# Google Sounds
+include $(CLEAR_VARS)
+LOCAL_MODULE := PrebuiltGoogleSounds
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := system/priv-app/PrebuiltGoogleSounds/PrebuiltGoogleSounds.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_PRODUCT_MODULE := true
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
+
